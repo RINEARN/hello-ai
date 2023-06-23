d8246f0a61f0c38f9b03a40f088aa8c00ac00023
RINEARN <commit@rinearn.com>
2023-06-23 10:53:47 +0900

    Add: a short code example
    For the experiment which examines how this code looks
    from AIs through their web-browsing feature.

---
+++ expt1/ShortCode.java
@@ -0,0 +1,9 @@
+/**
+ * A short code example, for the experiment which examines 
+ * how this code looks from AIs through their web-browsing feature.
+ */
+public class ShortCode {
+    public static void main(String[] args) {
+        System.out.println("Hello! Can you see this code?");
+    }
+}
